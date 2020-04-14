cb24c483569144a876c9806847d945de2cc634c7
JeishnaviChandra <mohithsekharamantri@msitprogram.net>
2020-04-14 11:30:50 +0530

    methods with simple PASS statement

--- lru.py
+++ lru.py
@@ -4,3 +4,9 @@ class lru:
         self.Data = []
         self.cache = {}
     
+    def put(self, key):
+        print("PASS put method")
+    def get(self, key, default = None):
+        print("PASS get method")
+    def get_cache(self):
+        print("PASS get_cache method")
